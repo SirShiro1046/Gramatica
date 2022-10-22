5f0b85e7d3b443c2b14bf3e6287851163ce49a30
SirShiro1046 <104867049+SirShiro1046@users.noreply.github.com>
2022-10-22 15:50:51 -0500

    quite for
    solo prueba de actualizacion para probar el push y el pull

--- src/main/java/com/mycompany/gramatica/Gramatica.java
+++ src/main/java/com/mycompany/gramatica/Gramatica.java
@@ -12,9 +12,7 @@ public class Gramatica {
     public static void main(String[] args) {
         System.out.println("sadads");
         
-        for (int i = 0; i < 10; i++) {
-            
-        }
+       
         
     }
 }
