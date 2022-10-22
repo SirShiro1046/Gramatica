636ade53cd935bf5463000c597a0dc29d13bd4dd
SirShiro1046 <myandroid146@gmail.com>
2022-10-22 15:49:22 -0500

    agrege for

--- src/main/java/com/mycompany/gramatica/Gramatica.java
+++ src/main/java/com/mycompany/gramatica/Gramatica.java
@@ -12,5 +12,9 @@ public class Gramatica {
     public static void main(String[] args) {
         System.out.println("sadads");
         
+        for (int i = 0; i < 10; i++) {
+            
+        }
+        
     }
 }
