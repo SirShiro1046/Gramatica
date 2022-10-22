bc8d690e66b2b5684dcb1e474e61b44ab0cfb4ac
SirShiro1046 <myandroid146@gmail.com>
2022-10-22 18:21:56 -0500

    agrege 18 funciones en la class gramatica

--- src/main/java/com/mycompany/gramatica/Gramatica.java
+++ src/main/java/com/mycompany/gramatica/Gramatica.java
@@ -9,5 +9,67 @@ package com.mycompany.gramatica;
  * @author myand
  */
 public class Gramatica {
+    String gramatica;
+
+    public Gramatica(String gramatica) {
+        this.gramatica = gramatica;
+    }
+    
+    public void declaracion(){
+        System.out.print("<declaracion> ->");
+        tipo(gramatica);
+    }
+    
+    public void tipo(String gramatica){
+        
+    }
+    public void entero(){
+        
+    }
+    public void flotante(){
+        
+    }
+    public void doble(){
+        
+    }
+    public void caracter(){
+        
+    }
+    public void lista_id(){
+        
+    }
+    public void id(){
+        
+    }
+    public void resto_id(){
+        
+    }
+    public void comilla(){
+        
+    }
+    public void igual(){
+        
+    }
+    public void letra(){
+        
+    }
+    public void digito(){
+        
+    }
+    public void digitoD(){
+     
+    }
+    public void valorI(){
+        
+    }
+    public void valorF(){
+        
+    }
+    public void valorD(){
+        
+    }
+    public void valorC(){
+        
+    }
     
 }
